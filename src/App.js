ab8bd081bdb59792ae79fd1862ae4887c40a0f36
Satya Sandeep <satyasandeep786@gmail.com>
2020-06-09 22:27:48 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Header</h1>
+                <h1>Body</h1>
+                <h1>Footer</h1>
+            </div>
+        )
+    }
+}
